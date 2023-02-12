8c1f26c65b280aa80d91c2426f8dee87a0c0f5d3
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-02-12 12:52:30 -0800

    updated data with headers to be simpler

--- Messenger App/client/Client.cs
+++ Messenger App/client/Client.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Management;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,34 +15,44 @@ namespace client
 {
     public partial class Client : Form
     {
+
+        private SimpleTcpClient _client;
+        private string _localComputerName;
+
+
+
         public Client()
         {
             InitializeComponent();
         }
 
-        private SimpleTcpClient _client;
-        
+
         private void Client_Load_1(object sender, EventArgs e)
         {
-            
+
             btnSend.Enabled = false;
-            
+            _localComputerName = GetLocalComputerName();
+
         }
 
         private void Events_DataReceived(object sender, DataReceivedEventArgs e)
         {
-            var message = Encoding.UTF8.GetString(e.Data.ToArray());
+            var messageReceived = Encoding.UTF8.GetString(e.Data.ToArray());
 
-            if (txtInfo.InvokeRequired)
+            // REQUESTNAME
+            if (messageReceived.Contains("REQUESTNAME"))
+            {
+                _client.Send("NAMEREQUEST+" + _localComputerName);
+            }
+            else
             {
                 this.Invoke((MethodInvoker)delegate
                 {
-                    txtInfo.Text += $@"{e.IpPort}: {Encoding.UTF8.GetString(e.Data.ToArray())}{Environment.NewLine}";
-
+                    txtInfo.Text +=
+                        $@"{e.IpPort}: {messageReceived}{Environment.NewLine}";
                 });
-
             }
-            
+
 
 
         }
@@ -49,7 +60,7 @@ namespace client
         private void Events_Disconnected(object sender, ConnectionEventArgs e)
         {
             this.Invoke((MethodInvoker)delegate {
-                
+
                 txtInfo.Text += $@"Server disconnected.{Environment.NewLine}";
                 btnSend.Enabled = false;
                 btnConnect.Enabled = true;
@@ -60,9 +71,9 @@ namespace client
         private void Events_Connected(object sender, ConnectionEventArgs e)
         {
             this.Invoke((MethodInvoker)delegate {
-                
+
                 txtInfo.Text += $@"Server connected.{Environment.NewLine}";
-            
+
             });
         }
 
@@ -78,7 +89,7 @@ namespace client
                 btnSend.Enabled = true;
                 btnConnect.Enabled = false;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, @"Message", MessageBoxButtons.OK, MessageBoxIcon.Error); ;
             }
@@ -87,9 +98,9 @@ namespace client
 
         private void btnSend_Click(object sender, EventArgs e)
         {
-            if(_client.IsConnected)
+            if (_client.IsConnected)
             {
-                if(!string.IsNullOrEmpty(txtMessage.Text))
+                if (!string.IsNullOrEmpty(txtMessage.Text))
                 {
                     _client.Send(txtMessage.Text);
                     txtInfo.Text += $@"Me: {txtMessage.Text}{Environment.NewLine}";
@@ -97,6 +108,11 @@ namespace client
                 }
             }
         }
-        
+
+        public string GetLocalComputerName()
+        {
+            return Dns.GetHostName();
+        }
+
     }
 }
--- Messenger App/tcp.server/server.cs
+++ Messenger App/tcp.server/server.cs
@@ -18,6 +18,7 @@ namespace tcp.server
     public partial class Server : Form
     {
         private SimpleTcpServer _server;
+        private Dictionary<string, string> _table;
 
 
         public Server()
@@ -28,9 +29,10 @@ namespace tcp.server
         private void Server_Load(object sender, EventArgs e)
         {
             txtIP.Text = GetLocalIpAddress() + @":9001";
+            _table = new Dictionary<string, string>();
             btnSend.Enabled = false;
         }
-        
+
 
 
         private void Events_ClientConnected(object sender, ConnectionEventArgs e)
@@ -38,47 +40,91 @@ namespace tcp.server
             this.Invoke((MethodInvoker)delegate
             {
                 txtInfo.Text += $@"{e.IpPort} connected.{Environment.NewLine}";
-                lstClientIP.Items.Add(e.IpPort);
-                
+                //lstClientIP.Items.Add(e.IpPort);
+
+                // [HEADER] + [MESSAGE]
+                _server.Send(e.IpPort, "REQUESTNAME");
+
             });
         }
 
         private void Events_ClientDisconnected(object sender, ConnectionEventArgs e)
         {
+            var ipAddressWithPort = e.IpPort;
+            var computerToRemove = string.Empty;
+
+
             this.Invoke((MethodInvoker)delegate
             {
                 txtInfo.Text += $@"{e.IpPort} disconnected.{Environment.NewLine}";
-                lstClientIP.Items.Remove(e.IpPort);
+                foreach (var item in _table)
+                {
+                    if (item.Key.Equals(ipAddressWithPort))
+                    {
+                        computerToRemove = item.Value;
+
+                    }
+                }
+
+                lstClientIP.Items.Remove(computerToRemove);
+                _table.Remove(ipAddressWithPort);
 
             });
         }
         private void Events_DataReceived(object sender, DataReceivedEventArgs e)
         {
-            
-            
-            
-            var message = Encoding.UTF8.GetString(e.Data.ToArray());
+            string messageReceived = Encoding.UTF8.GetString(e.Data.ToArray());
 
-            if (message.Contains("name"))
+            if (messageReceived.Contains("NAMEREQUEST"))
             {
-                string name = message.Remove(0, 3);
+                var clientComputerName = string.Empty;
+                char[] splitter = { '+' };
+                string[] messageSplit = messageReceived.Split(splitter, StringSplitOptions.RemoveEmptyEntries);
+
+                // NAMEREQUEST+COMPUTERNAME
+
+                foreach (var item in messageSplit)
+                {
+                    if (item.Equals("NAMEREQUEST"))
+                    {
+                        continue;
+                    }
+                    clientComputerName = item;
+
+                    // COMPUTERNAME
+
+                    _table.Add(e.IpPort, clientComputerName);
+
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        lstClientIP.Items.Add(clientComputerName);
+                    });
+
+
+                }
             }
-            this.Invoke((MethodInvoker)delegate 
+
+            else
             {
-                txtInfo.Text += $@"{e.IpPort}: {Encoding.UTF8.GetString(e.Data.ToArray())}{Environment.NewLine}";
-            });
-            
+                this.Invoke((MethodInvoker)delegate
+                {
+                    txtInfo.Text += $@"{e.IpPort}: {Encoding.UTF8.GetString(e.Data.ToArray())}{Environment.NewLine}";
+                });
+            }
+
         }
 
 
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            // loading server here allows user to change ip address
             _server = new SimpleTcpServer(txtIP.Text);
             _server.Events.ClientConnected += Events_ClientConnected;
             _server.Events.ClientDisconnected += Events_ClientDisconnected;
             _server.Events.DataReceived += Events_DataReceived;
             _server.Start();
+
             txtInfo.Text += $@"Starting...{Environment.NewLine}";
             btnStart.Enabled = false;
             btnSend.Enabled = true;
@@ -86,12 +132,25 @@ namespace tcp.server
         }
         private void btnSend_Click(object sender, EventArgs e)
         {
+            string ipConnection = string.Empty;
+
             if (_server.IsListening)
             {
                 // check if message box is empty or if no client is selected
-                if(!string.IsNullOrEmpty(txtMessage.Text) && lstClientIP.SelectedItem != null)
+                if (!string.IsNullOrEmpty(txtMessage.Text) && lstClientIP.SelectedItem != null)
                 {
-                    _server.Send(lstClientIP.SelectedItem.ToString(), txtMessage.Text);
+                    foreach (var item in _table)
+                    {
+                        // <socket> <computer name>
+                        if (item.Value == lstClientIP.SelectedItem.ToString())
+                        {
+                            ipConnection = item.Key;
+                        }
+                    }
+
+
+                    _server.Send(ipConnection, txtMessage.Text);
+
                     txtInfo.Text += $@"Server: {txtMessage.Text}{Environment.NewLine}";
                     txtMessage.Text = string.Empty;
                 }
@@ -99,10 +158,11 @@ namespace tcp.server
         }
 
         // get local host ip address
+        // populates ip address box for user
         public static string GetLocalIpAddress()
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
-            
+
             foreach (var ip in host.AddressList)
             {
                 if (ip.AddressFamily == AddressFamily.InterNetwork)
